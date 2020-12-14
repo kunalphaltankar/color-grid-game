e583980a15a25eb4d57add80242c1698d6d83275
Kunal Phaltankar <kunal.phaltankar@sarvasv.in>
2020-12-15 00:32:00 +0530

    Updates

---
+++ precache-manifest.7a3ff60613a1b493a82f7d07d83811db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d753c2d38d4b15687d07a277ae52364",
+    "url": "/color-grid-game/index.html"
+  },
+  {
+    "revision": "32cbbf29b541ae20fa8f",
+    "url": "/color-grid-game/static/css/main.d3dabe44.chunk.css"
+  },
+  {
+    "revision": "976bbec63ad85566fe30",
+    "url": "/color-grid-game/static/js/2.68f3adbf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/color-grid-game/static/js/2.68f3adbf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "32cbbf29b541ae20fa8f",
+    "url": "/color-grid-game/static/js/main.1f3226a1.chunk.js"
+  },
+  {
+    "revision": "8ebb70ba2dee6e60d15f",
+    "url": "/color-grid-game/static/js/runtime-main.adb57117.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/color-grid-game/precache-manifest.7a3ff60613a1b493a82f7d07d83811db.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/color-grid-game/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
